e6df82edbf226543eead3b8a0ff2e3aad09954ec
Semyon Zernov <st095228@student.spbu.ru>
2024-12-15 03:47:10 +0300

    Added R_O2_O calculations (#3)

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cmake . -B build -S . -DENABLE_TESTING=1 -DCMAKE_CXX_COMPILER=clang++
+cmake . -B build -S . -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=1 -DCMAKE_CXX_COMPILER=clang++
 cmake --build build --target=kappa++ -j 8
